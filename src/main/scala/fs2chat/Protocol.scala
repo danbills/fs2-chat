8ce50c3cc5c289019be5fdc9ed356b2bf91d0801
Dan Billings <dbillings@foundationmedicine.com>
2025-05-08 19:51:14 -0400

    update

--- src/main/scala/fs2chat/Protocol.scala
+++ src/main/scala/fs2chat/Protocol.scala
@@ -13,11 +13,6 @@ object Protocol:
   enum ClientCommand derives Codec:
     case RequestUsername(name: Username)
     case SendMessage(value: String)
-//  object ClientCommand:
-//    val codec: Codec[ClientCommand] = discriminated[ClientCommand]
-//      .by(uint8)
-//      .typecase(1, username.as[RequestUsername])
-//      .typecase(2, utf8_32.as[SendMessage])
 
   /** Base trait for messages sent from the server to the client. */
   enum ServerCommand derives Codec:
@@ -25,10 +20,3 @@ object Protocol:
     case Alert(text: String)
     case Message(name: Username, text: String)
     case Disconnect
-//  object ServerCommand:
-//    val codec: Codec[ServerCommand] = discriminated[ServerCommand]
-//      .by(uint8)
-//      .typecase(129, username.as[SetUsername])
-//      .typecase(130, utf8_32.as[Alert])
-//      .typecase(131, (username :: utf8_32).as[Message])
-//      .typecase(132, provide(Disconnect))
